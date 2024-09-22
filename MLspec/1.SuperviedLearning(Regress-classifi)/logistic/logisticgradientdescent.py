74e319c644963bf1a140bd10b42c113a9b8cf08a
Khoa Do <orewakhoa@gmail.com>
2024-09-22 23:29:02 +0700

    update logistic and linear folder

--- MLspec/1.SuperviedLearning(Regress-classifi)/logistic/logisticgradientdescent.py
+++ MLspec/1.SuperviedLearning(Regress-classifi)/logistic/logisticgradientdescent.py
@@ -1,8 +1,8 @@
 import numpy as np
 import copy, math
 import matplotlib.pyplot as plt
-from lossfunction import compute_cost_logistic
-from computegradient import compute_gradient_logistic
+from logisticlossfunction import compute_cost_logistic
+from logisticcomputegradient import compute_gradient_logistic
 X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
 def sigmoid(x):
